0d68f320cf9986077fa298efd853c04b2a9b2e41
Willa <willa@WilladeMacBook-Pro.local>
2024-01-25 10:49:04 +1300

    change classname

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MyDatePicker from "./Components/MyDatePicker";
 
 function App() {
   return (
-    <div className="App">
+    <div className="datePicker">
       <span>
         Choose Start Date: <MyDatePicker />
       </span>
