b735cbc8f13b84bb01d0650ef902fe2dd0eb0815
ZhenzhenShen <156273604+ZhenzhenShen@users.noreply.github.com>
2024-01-25 11:55:05 +1300

    add tables

--- src/Components/EventTable.js
+++ src/Components/EventTable.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 // Dummy data - you would replace this with real data passed in via props
@@ -39,36 +38,6 @@ const EventTable = () => {
             </td>
           </tr>
         ))}
-=======
-import React from "react";
-
-const EventTable = () => {
-  return (
-    <table className="eventTable">
-      <thead>
-        <tr>
-          <th>Event Name</th>
-          <th>Date</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Event 1</td>
-          <td>2024-01-25</td>
-          <td>Location A</td>
-        </tr>
-        <tr>
-          <td>Event 2</td>
-          <td>2024-01-26</td>
-          <td>Location B</td>
-        </tr>
-        <tr>
-          <td>Event 3</td>
-          <td>2024-01-27</td>
-          <td>Location C</td>
-        </tr>
->>>>>>> a4e16c37ab7df8edacc717906121b75af6b08c58
       </tbody>
     </table>
   );
