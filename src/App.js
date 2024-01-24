a4e16c37ab7df8edacc717906121b75af6b08c58
Willa2023 <suanxiaoyi2865@gmail.com>
2024-01-25 11:25:53 +1300

    add table

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyDatePicker from "./Components/MyDatePicker";
+import EventTable from "./Components/EventTable";
 
 function App() {
   return (
     <div className="datePicker">
-      <span>
-        Choose Start Date: <MyDatePicker />
-      </span>
-      <span>
-        Choose End Date: <MyDatePicker />
-      </span>
+      <div>
+        <span>
+          Choose Start Date: <MyDatePicker />
+        </span>
+        <span>
+          Choose End Date: <MyDatePicker />
+        </span>
+        <button>Search</button>
+      </div>
+
+      <>
+        <EventTable />
+      </>
     </div>
   );
 }
-
 export default App;
--- src/Components/EventTable.js
+++ src/Components/EventTable.js
@@ -1,1 +1,34 @@
-hello
+import React from "react";
+
+const EventTable = () => {
+  return (
+    <table className="eventTable">
+      <thead>
+        <tr>
+          <th>Event Name</th>
+          <th>Date</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Event 1</td>
+          <td>2024-01-25</td>
+          <td>Location A</td>
+        </tr>
+        <tr>
+          <td>Event 2</td>
+          <td>2024-01-26</td>
+          <td>Location B</td>
+        </tr>
+        <tr>
+          <td>Event 3</td>
+          <td>2024-01-27</td>
+          <td>Location C</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default EventTable;
