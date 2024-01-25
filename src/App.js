48a784041df1f158be531a03e33844fae34d5c29
Willa2023 <suanxiaoyi2865@gmail.com>
2024-01-25 13:20:16 +1300

    add path in appjs

--- src/App.js
+++ src/App.js
@@ -2,23 +2,48 @@ import logo from "./logo.svg";
 import "./App.css";
 import MyDatePicker from "./Components/MyDatePicker";
 import EventTable from "./Components/EventTable";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-    <div className="datePicker">
-      <span>
-        Choose Start Date: <MyDatePicker />
-      </span>
-      <span>
-        Choose End Date: <MyDatePicker />
-      </span>
-    </div>
-    <div>
-      <EventTable />
-    </div>
-    </div>
-
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Define your routes here */}
+          <Route path="/" element={<Event />} />
+          <Route path="/edit" element={<EditInfo />} />
+          <Route path="/add" element={<AddInfo />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+// Define components for each route
+function Event() {
+  return (
+    <div>
+      <div className="datePicker">
+        <span>
+          Choose Start Date: <MyDatePicker />
+        </span>
+        <span>
+          Choose End Date: <MyDatePicker />
+        </span>
+      </div>
+      <div>
+        <EventTable />
+      </div>
+    </div>
+  );
+}
+
+function EditInfo() {
+  return <div>Edit Page</div>;
+}
+
+function AddInfo() {
+  return <div>Add Page</div>;
+}
+
 export default App;
