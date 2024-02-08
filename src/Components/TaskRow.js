f2b7cca9b76fc92e1cc416ec1bc9db7cbd9854bd
ZhenzhenShen <156273604+ZhenzhenShen@users.noreply.github.com>
2024-02-08 13:06:26 +1300

    taskrow

--- src/Components/TaskRow.js
+++ src/Components/TaskRow.js
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const TaskRow = ({ task, onDelete }) => {
-  const handleDelete = () => {
-    onDelete(task.taskID);
-  };
+
 
   return (
     <tr>
@@ -20,7 +18,7 @@ export const TaskRow = ({ task, onDelete }) => {
         <Link to={`/edit/${task.taskID}`}>
           <button className="edit-button">Edit</button>
         </Link>
-        <button className="delete-button" onClick={handleDelete}>
+        <button className="delete-button" onClick={() => onDelete(task.taskID)}>
           Delete
         </button>
       </td>
